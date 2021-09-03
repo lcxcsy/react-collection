1b740db1e303422563ca49adf9e86765df672abd
Chenxi-Liu <329052927@qq.com>
2021-09-03 17:36:46 +0800

    update: react-router

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,4 @@
-import ErrorPage from '@/components/Error'
+import ErrorPage from '@/components/ErrorPage'
 import React, { Component } from 'react'
 
 export default class Error extends Component {
--- src/router.config.js
+++ src/router.config.js
@@ -1,18 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2021-09-03 16:37:45
- * @LastEditTime: 2021-09-03 17:03:58
+ * @LastEditTime: 2021-09-03 17:33:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \quick-react\src\router.config.js
  */
 import Home from '@/pages/Home'
+import Error from '@/pages/Error'
 
 const routers = [
   {
     path: '/', /** 默认页面 */
     component: Home,
     exact: true /** 是否为严格模式 */
+  },
+  {
+    path: '/error',
+    exact: true,
+    component: Error
   },
   {
     path: '/home',
